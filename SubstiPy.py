101c81fddce19e275ecf5e58543447a59f1af0ed
ZarkedSec <65082888+ZarkedSec@users.noreply.github.com>
2020-08-04 12:40:07 +0200

    New feature: Autocompletion

--- SubstiPy.py
+++ SubstiPy.py
@@ -3,6 +3,8 @@
 import sys
 import string
 from os import system, path
+from math import log10 as log
+from random import shuffle, randint
 
 class Color:
 	GREEN = "\033[32m"
@@ -32,7 +34,7 @@ text = original_text
 changed_index = [] # Index of changed letters
 changed_letters = [] # Original letters
 changed_letters_in = [] # New letters
-replaced_letters = list(string.ascii_uppercase) # New letters + others
+replaced_letters = list(string.ascii_uppercase) # New letters + others (table)
 punctuation = ["-","'"]
 
 class menu:
@@ -164,11 +166,127 @@ exit ........................... Leave the program
 				del found[found.index(i)]
 		return found
 		print("\n")
-	def auto():
-		print("ok")
 	def exit():												# exit function
 		print("\n")
 		exit()
+
+class autocompletion:
+	def RighFormat(text):
+	    text = (text.upper()).replace(" ","")
+	    for char in list(text) :
+	        if not(char in string.ascii_uppercase):
+	        	text = text.replace(char, "")
+	    return text
+
+	def QuadgramsN(file):
+	    quadgrams = {}
+	    N = 0
+	    for i in range(0, len(file)-1):
+	        file[i] = file[i].split(" ")
+	        quadgrams[file[i][0]] = float(file[i][1])
+	        N += int(file[i][1])
+	    return quadgrams, N
+
+	def Fitness(text, quadgrams, N):
+	    fitness = 0
+	    for i in range(0, len(text)-3):
+	        try:
+	            fitness += log(quadgrams[text[i:i+4]]/N)
+	        except:
+	            fitness += log(0.01/N)
+	    return fitness
+
+	def Substitute(TEXT, ALPHABET, SUBSTITUTED_ALPHABET):
+	    substituted_text = list(TEXT)
+	    for i in range(0, len(TEXT)):
+	        substituted_text[i] = SUBSTITUTED_ALPHABET[ALPHABET.index(substituted_text[i])]
+	    return ''.join(substituted_text)
+
+	def Break(TEXT, ALPHABET, QUADGRAMS, N, limit):
+	    TEXT_RIGHT_FORMAT = TEXT
+	    text_sample = TEXT
+
+	    key = ALPHABET[:]
+
+	    final_key = []
+	    final_fitness = -9e+10
+
+	    endless = 0
+
+	    system("clear && printf '\e[3J'")
+	    print("["+"#"*int(endless*30/limit)+" "*int(30/limit*(limit-endless))+"] "+str(endless)+" / "+str(limit))
+
+	    while endless < limit:
+	        shuffle(key)
+	        loop = True
+	        best_fitness = -9e+10
+	        best_key = []
+	        while loop:
+	            loop = False
+	            for a in range(0, 26):
+	                for b in range(0, 26):
+	                    new_key = key[:]
+	                    new_key[a], new_key[b] = new_key[b], new_key[a]
+	                    text_sample = autocompletion.Substitute(TEXT_RIGHT_FORMAT, ALPHABET, new_key)
+	                    fitness = autocompletion.Fitness(text_sample, QUADGRAMS, N)
+	                    if fitness > best_fitness:
+	                        best_fitness = fitness
+	                        best_key = new_key[:]
+	                        best_text = text_sample
+	                        loop = True
+	            key = best_key[:]
+	        if final_fitness < best_fitness:
+	            final_fitness = best_fitness
+	            final_key = best_key
+	            final_text = best_text
+	            endless = 0
+	        else :
+	            endless += 1
+	        system("clear && printf '\e[3J'")
+	        print("["+"#"*int(endless*30/limit)+" "*int(30/limit*(limit-endless))+"] "+str(endless)+" / "+str(limit))
+	    return final_key, final_fitness, final_text
+
+	def BackToNormal(TEXT, final_text):
+	    final_text = list(final_text)
+	    text = list(TEXT)
+
+	    for i in range(0, len(text)):
+	        if text[i].islower() :
+	            final_text[i] = final_text[i].lower()
+	        elif not(text[i] in string.ascii_uppercase):
+	            final_text = final_text[:i]+[text[i]]+final_text[i:]
+	    return ''.join(final_text)
+
+system("clear")
+
+yn_autocompletion = input("\nDo you want to try the autocompletion? [Y/N] (Default: N) : ").upper()
+
+if yn_autocompletion == "Y" or yn_autocompletion == "YES":
+
+	ALPHABET = list(string.ascii_uppercase)
+	FILE = (open("src/english_quadgrams.txt","r").read()).split("\n")
+
+	text = autocompletion.RighFormat(original_text)
+	QUADGRAMS, N = autocompletion.QuadgramsN(FILE)
+	system("clear && printf '\e[3J'")
+	limit = input(Color.RED+"\n\n--------------------------------------------------------------\n\n - The more loops you do, the more chances you have to find the clear text\n   (It can take up to several minutes for a long text).\n\n - This script does not have a 100% success rate,\n   you might have to launch it several times before finding your solution\n\n--------------------------------------------------------------"+Color.WHITE+"\n\nHow many loops do you want to make? (Default: 5) : ")
+	try:
+		limit = int(limit)
+	except:
+		limit = 5
+	key, fitness, text = autocompletion.Break(text, ALPHABET, QUADGRAMS, N, limit)
+	text = autocompletion.BackToNormal(original_text, text)
+
+	replaced_letters = key[:]
+	for i in range(0, len(replaced_letters)):
+		replaced_letters[i] = Color.GREEN+replaced_letters[i]+Color.WHITE
+
+	changed_letters = ALPHABET[:]
+	changed_letters_in = key[:]
+
+	for i in range(0, len(text)):
+		if text[i] in ALPHABET or text[i] in list(string.ascii_lowercase) :
+			changed_index += [i]
 
 menu.clear(text, changed_index, replaced_letters)
 
@@ -182,12 +300,14 @@ while True:
 		elif ((cmd.lower()).split())[0] == "replace" :		# replace input
 			cmd = ((cmd.lower()).split())
 			if len(cmd) == 3:
-				if cmd[1].lower() in string.ascii_lowercase and cmd[2].lower() in string.ascii_lowercase :
+				if cmd[1].upper() in string.ascii_uppercase and cmd[2].upper() in string.ascii_uppercase :
+					cmd[1] = cmd[1].upper()
+					cmd[2] = cmd[2].upper()
 					if cmd[1] in changed_letters:
-						print(Color.WHITE+"You already changed the letter '"+cmd[1].lower()+"' in '"+changed_letters_in[changed_letters.index(cmd[1])]+"'")
+						print(Color.WHITE+"You already changed the letter '"+cmd[1].lower()+"' in '"+changed_letters_in[changed_letters.index(cmd[1])].lower()+"'")
 						continue
 					elif cmd[2] in changed_letters_in:
-						print(Color.WHITE+"You already changed the letter '"+changed_letters[changed_letters_in.index(cmd[2])]+"' in '"+cmd[2].lower()+"'")
+						print(Color.WHITE+"You already changed the letter '"+changed_letters[changed_letters_in.index(cmd[2])].lower()+"' in '"+cmd[2].lower()+"'")
 						continue
 					changed_letters += [cmd[1]]
 					changed_letters_in += [cmd[2]]
